5c15ec1adbb9b65e74730137c371bd3cb1c3450f
IrisLong <larissa@zen-game.cn>
2023-02-28 20:40:35 +0800

    ✨feat(custom): 404页面

---
+++ src/layouts/404.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <el-empty description="抱歉，页面不存在！" :image-size="500">
+    <!-- <el-button type="primary" @click="$router.back()">返回</el-button> -->
+  </el-empty>
+</template>

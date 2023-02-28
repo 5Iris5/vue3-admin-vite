83ba92d8e3c86aa0e497a361ddd9796d13cc16d7
IrisLong <larissa@zen-game.cn>
2023-02-28 20:32:53 +0800

    🐞fix(custom): vite.config.ts文件使用import报eslint错误

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 8086,
+    open: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://oat.365you.com/:90/',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
